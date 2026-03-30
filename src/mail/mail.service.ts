3e9d0f409c3a4c06a6ce7ea19e3c6b9bc20e6a72
Fllunn <dmitrijpohoda2940@gmail.com>
2026-03-30 15:45:04 +0500

    add sendVerificationCode in mail module

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,5 +1,6 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
+import ApiError from 'src/exceptions/errors/api-error';
 
 // types
 import type { User } from 'src/user/interfaces/user.interface'
@@ -8,13 +9,15 @@ import type { User } from 'src/user/interfaces/user.interface'
 export class MailService {
   constructor(private mailerService: MailerService) { }
 
+  private readonly APP_NAME = 'DocumentFill'
+  private readonly MAIL_USER = process.env.MAIL_USER
 
   public async sendJobFormCreatedNotification(email: string, name: string) {
     let registerUrl = `${process.env.CLIENT_URL}/registration/employee?email=${email}`
 
     await this.mailerService.sendMail({
       to: email,
-      from: '"DocumentFill" <dmitrijpohoda2960@gmail.com>',
+      from: `"${this.APP_NAME}" <${this.MAIL_USER}>`,
       subject: 'DocumentFill: анкета создана',
       template: './job-form-created',
       context: {
@@ -28,7 +31,7 @@ export class MailService {
     // const url = `example.com/auth/confirm?token=${token}`;
     await this.mailerService.sendMail({
       to: user.email,
-      from: '"DocumentFill" <dmitrijpohoda2960@gmail.com>',
+      from: `"${this.APP_NAME}" <${this.MAIL_USER}>`,
       subject: 'DocumentFill: регистрация успешна',
       template: './confirmation',
       context: {
@@ -39,23 +42,50 @@ export class MailService {
     });
   }
 
-  public async sendOrderNotifications(userEmails: string[], order: any) {
-    return await this.mailerService.sendMail({
-      to: userEmails,
-      from: '"DocumentFill" <dmitrijpohoda2960@gmail.com>',
-      subject: 'DocumentFill: новый заказ',
-      template: 'order',
-      context: { order: order._doc }
-    });
-  }
-
   public async sendResetLink(link: string, email: string) {
     return await this.mailerService.sendMail({
       to: email,
-      from: '"DocumentFill" <dmitrijpohoda2960@gmail.com>',
+      from: `"${this.APP_NAME}" <${this.MAIL_USER}>`,
       subject: 'DocumentFill: восстановление пароля',
       template: 'reset-pasword',
       context: { link }
     });
+  }
+
+  public async sendVerificationCode(email: string, code: string, type: string) {
+    let subject = ''
+    let title = ''
+
+    switch (type) {
+      case 'email-verification':
+        subject = `${this.APP_NAME}: код для подтверждения почты`
+        title = 'Ваш код для подтверждения почты'
+        break
+      case 'password-reset':
+        subject = `${this.APP_NAME}: код для сброса пароля`
+        title = 'Ваш код для сброса пароля'
+        break
+      case 'enable-2fa':
+        subject = `${this.APP_NAME}: код для включения двухфакторной аутентификации`
+        title = 'Ваш код для включения двухфакторной аутентификации'
+        break
+      case 'disable-2fa':
+        subject = `${this.APP_NAME}: код для отключения двухфакторной аутентификации`
+        title = 'Ваш код для отключения двухфакторной аутентификации'
+        break
+      default:
+        throw ApiError.BadRequest('Неверный тип кода')
+    }
+
+    await this.mailerService.sendMail({
+      to: email,
+      from: `"${this.APP_NAME}" <${this.MAIL_USER}>`,
+      subject,
+      template: './verification-code',
+      context: {
+        title,
+        code
+      }
+    })
   }
 }
